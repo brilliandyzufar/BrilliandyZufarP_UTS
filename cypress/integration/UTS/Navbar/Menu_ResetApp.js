9e6c3c98e4152e3504d350dc05e30ac3e6e61c87
brilliandyzufar <brilliandy12@gmail.com>
2021-10-31 12:18:13 +0700

    Brilliandy Zufar Pangestu-1941720201
    Signed-off-by: brilliandyzufar <brilliandy12@gmail.com>

---
+++ cypress/integration/UTS/Navbar/Menu_ResetApp.js
@@ -0,0 +1,15 @@
+const frame = document.getElementById('your-frame-id');
+
+context ('Saucedemo Login Test', () =>{
+    it('Login Saucedemo', () =>{
+        cy.visit('https://saucedemo.com')
+        cy.get('[id="user-name"]').type('standard_user')
+        cy.get('input[type="password"]').type('secret_sauce')
+        cy.get('[id="login-button"]').click()
+        //Navbar//
+        cy.get('#react-burger-menu-btn').click() //burger button
+        cy.get('#reset_sidebar_link').click() //ResetAPP
+
+    
+    })
+})
